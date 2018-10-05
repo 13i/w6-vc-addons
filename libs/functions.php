0e4082fa0f154ccb85600dfc10460a6961f826c1
Treize Interactif <pierre@13i.fr>
2018-10-05 18:13:25 +0200

    Init

---
+++ libs/functions.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Functions
+ *
+ * @package   w6\vc_addons
+ * @since     1.0.0
+ * @author    WEB6 <contact@web6.fr>
+ * @copyright 2018 WEB6
+ * @license   https://www.gnu.org/licenses/gpl-3.0.txt  GNU GPLv3
+ * @link      https://github.com/web6-fr/w6-vc-addons
+ */
+
+namespace W6\Vc_Addons;
+
+/**
+ * Autoload
+ *
+ * @param string $class_name Class name.
+ */
+function autoload( $class_name ) {
+
+	if ( false === strpos( $class_name, 'W6\Vc_Addons' ) ) {
+		return;
+	}
+
+	$parts = explode( '\\', $class_name );
+	$parts = array_filter($parts);
+
+	$basename = array_pop( $parts );
+	$basename = str_replace( '_', '-', $basename );
+	$basename = strtolower( $basename );
+
+	$path = implode( '/', $parts );
+	$path = str_replace( 'W6/Vc_Addons', 'libs', $path );
+	$path = ROOT . '/' . $path;
+
+	$types = array(
+		'class',
+		'trait',
+		'interface',
+	);
+
+	foreach ( $types as $type ) {
+		$_path = $path . '/' . $type . '-' . $basename . '.php';
+		if ( file_exists( $_path ) ) {
+			require_once $_path;
+			break;
+		}
+	}
+}
---
+++ w6-wp-addons.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Visual Composer Addons
+ *
+ * Useful addons for Visual Composer :
+ * - Before & After with spots
+ *
+ * @package   w6\vc_addons
+ * @since     1.0.0
+ * @author    WEB6 <contact@web6.fr>
+ * @copyright 2018 WEB6
+ * @license   https://www.gnu.org/licenses/gpl-3.0.txt  GNU GPLv3
+ * @link      https://github.com/web6-fr/w6-vc-addons
+ *
+ * @wordpress-plugin
+ * Plugin Name:   W6 Visual Composer Addons
+ * Plugin URI:    https://github.com/web6-fr/w6-vc-addons
+ * Description:   Useful addons for Visual Composer
+ * Version:       1.0.0
+ * Author:        WEB6
+ * Author URI:    https://web6.fr
+ * License:       GPL-3.0
+ * License URI:   https://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:   w6-vc-addons
+ * Domain Path:   /languages
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Plugin version.
+ */
+define( 'W6\Vc_Addons\VERSION', '1.0.0' );
+
+/**
+ * Plugin root folder.
+ */
+define( 'W6\Vc_Addons\ROOT', dirname( __FILE__ ) );
+
+/**
+ * Functions
+ */
+require_once 'libs/functions.php';
+
+/**
+ * Autoload
+ */
+spl_autoload_register( '\W6\Vc_Addons\autoload' );
+
+/**
+ * Composer
+ */
+require_once 'vendor/autoload.php';
+
+/**
+ * Init plugin
+ */
+\W6\Vc_Addons\Vc_Addons::init();
